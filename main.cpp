f554ed8a7cb171c33cddd4cf956860e7f66243e7
tanis <tk009@lvc.edu>
2024-11-21 16:26:34 -0500

    Create the Log class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <memory>
+#include <mutex>
 
-int main(int, char**){
-    std::cout << "Hello, from hw!\n";
+// Class Log
+class Log {
+public:
+    // Get instance of Log
+    static Log& getInstance() {
+        // Lock guard
+        std::lock_guard<std::mutex> lock(mutex);
+        // IF there is no instance, create instance
+        if (!instance) {
+            instance = std::make_unique<Log>();
+        }
+        // Return the instance
+        return *instance;
+    }
+
+    // Log a message
+    void log(const std::string& message) {
+        std::cout << "Log: " << message << std::endl;
+    }
+
+    // Delete the copy constructor
+    Log(const Log&) = delete;
+    // Delete the copy assignment operator
+    Log& operator=(const Log&) = delete;
+
+private:
+    // Instance of the Log
+    static std::unique_ptr<Log> instance;
+    // Mutex for thread safety
+    static std::mutex mutex;
+    // Private constructor to prevent instantiation
+    Log() = default;
+};
+
+int main() {
+    
 }
